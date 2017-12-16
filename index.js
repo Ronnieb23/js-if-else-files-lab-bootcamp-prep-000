e6b993e1762accd58d9d27cd4e87f613d9ce79b5
Ronald Bennett <bennettrd318@gmail.com>
2017-12-16 14:22:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var height = "74"
 if (name === "susan"){
   console.log("The name is correct")
 }
-else (name === "Susan") {
+else { 
+  (name === "Susan") 
   console.log("Expected the name to be Susan")
 }
 // Don't worry about this
